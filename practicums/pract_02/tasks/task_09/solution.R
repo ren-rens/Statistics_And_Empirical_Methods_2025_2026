edd2abe8083c19e2695547b7bcabad868b926b48
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-14 12:55:58 +0300

    Update solution.R

--- practicums/pract_02/tasks/task_09/solution.R
+++ practicums/pract_02/tasks/task_09/solution.R
@@ -1,6 +1,8 @@
 simulate.questions <- function() {
     x <- sample(c(0, 0, 0, 1), 10, replace=T) # wrong answers = 0, right = 1
-    
+    # x <- sample(c(0, 1), 10, replace=T, prob=c(0.75, 0.25)
+    # giving probability of the answers also works
+
     sum(x)
 }
 
