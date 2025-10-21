a9b7ed2fa66397a2921c6bcc0a67f76140dae92b
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-21 13:34:32 +0300

    Create solution.R

---
+++ practicums/pract_03/tasks/task_21/solution.R
@@ -0,0 +1,11 @@
+# Task 21
+# X = count of new batteries from the chosen
+# P(do not work) = 1 - P(X == 2)
+# P(X == 2) = chosing 2 from the new batteries (5) and zero from the old (3) / choosing 2 from all batteries (8)
+1 - choose(5, 2) *  choose(3, 0) / choose(8, 2)
+
+k = 2
+M = 5
+N = 8
+n = 2
+1 - dhyper(k, M, N - M, n)
