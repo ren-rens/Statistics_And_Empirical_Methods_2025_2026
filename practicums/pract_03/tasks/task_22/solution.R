70afc8c3945f65b5e38d554b64cedd7a747fa1bd
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-10-21 13:45:28 +0300

    Update solution.R

--- practicums/pract_03/tasks/task_22/solution.R
+++ practicums/pract_03/tasks/task_22/solution.R
@@ -11,10 +11,16 @@ n <- 1500
 k = 2
 pbinom(k, n, p)
 
+# ~=
+ppois(2, n * p)
+
 # B) 1 to 3 errors per 5 pages
 # P(1 <= X <= 3) = P(X <= 3) - P(X <= 0)
 pbinom(3, n, p) - pbinom(0, n, p)
+ppois(3, n * p) - ppois(0, n * p)
+
 
 # P(1 <= X <= 3) = sum(dbinom(1:3, n, p)
 k <- 1:3
 sum(dbinom(k, n, p))
+
