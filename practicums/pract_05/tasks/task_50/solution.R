1d65aa80ac40e756e84a7edd2a35c7689b57d439
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-11-04 13:32:52 +0200

    Create solution.R

---
+++ practicums/pract_05/tasks/task_50/solution.R
@@ -0,0 +1,22 @@
+# 49
+library(MASS)
+attach(survey)
+
+table(Pulse)
+
+pulse.interval <- cut(Pulse, breaks = seq(30, 110, 10))
+# pulse.interval
+
+table(pulse.interval)
+
+# graphic
+barplot(table(pulse.interval))
+
+hist(Pulse)
+hist(Pulse, breaks = seq(30, 110, 10))
+
+# closest to the first tale is:
+stripchart(Pulse, method = "stack", pch = 20)
+stripchart(Pulse, method = "stack", pch = 18)
+stripchart(Pulse, method = "stack", pch = 1)
+stripchart(Pulse, method = "stack", pch = "*")
